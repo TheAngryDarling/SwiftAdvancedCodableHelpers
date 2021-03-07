7675036b458154795b2d580443de669001ce4206
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2021-03-07 16:44:10 -0500

    Removed  CodableKey from package and did a export import struct BasicCodableHelpers.CodableKey as CodableKey was moved to BasicCodableHelpers a while ago but the move was not reflected within this package until now The exported import is used to not break old code where only importing AdvancedCodableHelpers to access CodableKey

--- Sources/AdvancedCodableHelpers/codable/CodableKey.swift
+++ Sources/AdvancedCodableHelpers/codable/CodableKey.swift
@@ -4,68 +4,9 @@
 //
 //  Created by Tyler Anger on 2018-11-03.
 //
-// Based off swift code https://github.com/apple/swift/blob/master/stdlib/public/core/Codable.swift.gyb)
 
 import Foundation
-
-/// A basic structure used to define a coding key
-public struct CodableKey: CodingKey {
-    
-    /// The string to use in a named collection (e.g. a string-keyed dictionary).
-    public var stringValue: String
-    /// The value to use in an integer-indexed collection (e.g. an int-keyed dictionary).
-    public var intValue: Int?
-    
-    /// Creates a new instance from the given string.
-    ///
-    /// - parameter stringValue: The string value of the desired key.
-    public init(stringValue: String) {
-        self.stringValue = stringValue
-        self.intValue = nil
-    }
-    
-    /// Creates a new instance from the specified integer.
-    ///
-    /// - parameter intValue: The integer value of the desired key.
-    public init(intValue: Int) {
-        self.stringValue = "\(intValue)"
-        self.intValue = intValue
-    }
-    
-    /// Creates a new instance from the given string and integer.
-    ///
-    /// - Parameters
-    ///   - stringValue: The string value of the desired key.
-    ///   - intValue: The integer value of the desired key.
-    public init(stringValue: String, intValue: Int?) {
-        self.stringValue = stringValue
-        self.intValue = intValue
-    }
-    
-    /// Creates a new instance from the specified integer.
-    ///
-    /// - parameter index: The integer value of the desired key.
-    public init(index: Int) {
-        self.stringValue = "Index \(index)"
-        self.intValue = index
-    }
-    
-    internal func properHashValue<HT>(forType tp: HT.Type) -> HT where HT: DictionaryKeyCodable {
-        if let iV = self.intValue, type(of: iV) == tp {
-            return iV as! HT
-        } else if type(of: self.stringValue) == tp {
-            return self.stringValue as! HT
-        } else {
-            fatalError("Unable to cast to type \(tp)")
-        }
-    }
-    
-}
-
-#if !_runtime(_ObjC)
-extension CodableKey: CustomStringConvertible {
-    public var description: String {
-        return "\(type(of: self)).\(self.stringValue)"
-    }
-}
-#endif
+// This was done becase CodableKey was copied into BasicCodableHelpers a while aog
+// and not removed from here which could cause ambigious reference
+// when importing both packages
+@_exported import struct BasicCodableHelpers.CodableKey
